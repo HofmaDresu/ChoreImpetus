cc607b7b61f9cd475658bde9aee0d91b5d633343
mhoffman <>
2013-05-03 19:12:15 -0400

    Update for release build

--- ChoreImpetusAndroid/Properties/AssemblyInfo.cs
+++ ChoreImpetusAndroid/Properties/AssemblyInfo.cs
@@ -18,11 +18,17 @@ using Android.App;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyVersion("0.2.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
 
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
+
+#if !DEBUG 
+[assembly: Application(Debuggable=false)] 
+#else
+[assembly: Application(Debuggable=true)]
+#endif
 
